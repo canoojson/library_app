d5b9aede952964e942533db89c2500b69dd3f14f
Sergio Cano <Sergio.Cano.Pomer.2004@gmail.com>
2025-01-10 13:31:50 +0100

    Initial Commit

---
+++ __manifest__.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Library Management Application',
+
+    'description': 'manage library book catalogue and lending',
+
+    'author': "Sergio Cano",
+    'website': "http://www.yourcompany.com",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/15.0/odoo/addons/base/data/ir_module_category_data.xml
+    # for the full list
+    'category': 'Uncategorized',
+    'license': 'AGPL-3',
+    'depends': ['base'],
+    'application': True,
+    'version': '0.1',
+
+    # any module necessary for this one to work correctly
+    'depends': ['base'],
+
+    # always loaded
+    'data': [
+        # 'security/ir.model.access.csv',
+        'views/library_menu.xml',
+        'views/templates.xml',
+    ],
+    # only loaded in demonstration mode
+    'demo': [
+        'demo/demo.xml',
+    ],
+}
